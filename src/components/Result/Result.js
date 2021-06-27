ec602e21eb22d07bc7ef210c8390e05afc6a0697
olgaostapchuka11 <olya.zaiceva91@gmail.com>
2021-06-27 15:58:52 +0300

    Create Result section

---
+++ src/components/Result/Result.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./result.css";
+
+import "antd/dist/antd.css";
+
+const Result = () => {
+  return (
+    <div className="result-section">
+      <h1 className="result-section-title">Calculation result</h1>
+    </div>
+  );
+};
+
+export default Result;
