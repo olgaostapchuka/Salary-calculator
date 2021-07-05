51a048301861cc68356b8bd69af040e15d878c0e
olgaostapchuka11 <olya.zaiceva91@gmail.com>
2021-07-05 20:04:15 +0300

    Add personStatus function

--- src/components/PersonStatus/PersonStatus.js
+++ src/components/PersonStatus/PersonStatus.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Checkbox } from "antd";
 import "./personStatus.css";
-import "antd/dist/antd.css";
+import { DataContext } from "../../providers/CalculatorProvider";
 
 const PersonStatus = () => {
+  const { setPersonStatus } = useContext(DataContext);
+
+  const enablePersonStatus = (value) => {
+    setPersonStatus((value) => !value);
+  };
   return (
     <div className="person-status">
-      <Checkbox />
-      <label className="person-status-label">
+      <Checkbox
+        id="personStatus"
+        defaultChecked={false}
+        onChange={enablePersonStatus}
+      />
+      <label className="person-status-label" htmlFor="personStatus">
         Politically repressed person or national resistance movement status
         person
       </label>
