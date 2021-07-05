5703ff16fdbe23886c176855349de3c53d680c1e
olgaostapchuka11 <olya.zaiceva91@gmail.com>
2021-07-05 19:20:15 +0300

    Add direction function

--- src/components/Direction/Direction.js
+++ src/components/Direction/Direction.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./direction.css";
-import "antd/dist/antd.css";
 import { Radio } from "antd";
+import { DataContext } from "../../providers/CalculatorProvider";
 
 const Direction = () => {
+  const { setDirection } = useContext(DataContext);
+
+  const bruttoNettoMode = (value) => {
+    setDirection((value) => !value);
+  };
+
   return (
     <div className="direction">
       <label className="direction-label">Direction</label> 
-      <Radio.Group defaultValue="GrossNet" buttonStyle="solid">
+      <Radio.Group
+        defaultValue="GrossNet"
+        onChange={bruttoNettoMode}
+        buttonStyle="solid"
+      >
         <Radio.Button value="GrossNet">Gross &gt; Net Salary</Radio.Button>
         <Radio.Button value="NetGross">Net &gt; Gross Salary</Radio.Button>
       </Radio.Group>
