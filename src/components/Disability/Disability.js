ed24f367d92edf61e0725ae0f8eb520de6cd41d5
olgaostapchuka11 <olya.zaiceva91@gmail.com>
2021-07-05 19:27:19 +0300

    Add disability function

--- src/components/Disability/Disability.js
+++ src/components/Disability/Disability.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./disability.css";
-import "antd/dist/antd.css";
 import { Select } from "antd";
+import { DataContext } from "../../providers/CalculatorProvider";
 const { Option } = Select;
 
 const Disability = () => {
+  const { setDisability } = useContext(DataContext);
+
+  function onChangeDisability(value) {
+    setDisability(value);
+  }
   return (
     <div className="disability">
       <label className="disability-label" htmlFor="disabilitySelect">
@@ -15,7 +20,9 @@ const Disability = () => {
         id="disabilitySelect"
         defaultValue="not"
         className="disability-select"
+        onChange={onChangeDisability}
       >
+        <Option value="not">not</Option>
         <Option value="first_second_group">1st or 2nd group</Option>
         <Option value="third_group">3rd group</Option>
       </Select>
